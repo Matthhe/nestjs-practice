9daee983946907458d3150691f3f5e5e9cecf5c6
Matthew <lojsamatvej@gmail.com>
2026-04-21 19:54:24 +0300

    added ParseIntType

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 @Controller('users') // /users - route
 export class UsersController {
@@ -12,34 +12,34 @@ export class UsersController {
      PATCH /users/:id - bc i want to change only one thing in my user
      DELETE /users/:id
     */
+     //! Routes work like a waterfall. 
+     //! route like 'interns'(expample) must be before a dynamic route that could accept and ID as a param
 
-     // Routes work like a waterfall. 
-     // route like 'interns'(expample) must be before a dynamic route that could accept and ID as a param
-
-    @Get() // GET /users or /users?role=value where ?role=value => query parameter
+    @Get() //* GET /users or /users?role=value where ?role=value => query parameter
     findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
         return this.usersService.findAll(role)
     }
 
-    @Get(':id') // GET /users/:id 
-    findOne(@Param('id') id: string){
-        return this.usersService.findOne(+id)
+    //! In this method used ParseIntPipe, which give us apportunity to not use '+' before 'id'
+    @Get(':id') //* GET /users/:id 
+    findOne(@Param('id', ParseIntPipe) id: number){
+        return this.usersService.findOne(id)
     }
 
-    @Post() // POST /users and there @Body have user information
+    @Post() //* POST /users and there @Body have user information
     create(@Body() user: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }){ // to POST we need to read Body of the request
         return this.usersService.create(user) // bc we need tosend that data 
                               // 'user' here, it is what type the body is
     }
     
-    @Patch(':id') // PACTH /users/:id 
-    update(@Param('id') id: string, @Body() userUpdate: {name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }){
-        return this.usersService.update(+id, userUpdate)
+    @Patch(':id') //* PACTH /users/:id 
+    update(@Param('id', ParseIntPipe) id: number, @Body() userUpdate: {name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }){
+        return this.usersService.update(id, userUpdate)
     }
 
-    @Delete(':id') // DELETE /users/:id 
-    delete(@Param('id') id: string){
-        return this.usersService.delete(+id)
+    @Delete(':id') //* DELETE /users/:id 
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.usersService.delete(id)
     }
 }
 
